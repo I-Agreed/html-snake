a35e5c70385923f092c0d535d474e5829e9c199e
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2020-06-15 10:14:41 +1000

    started snake class
    and added an id to canvas

---
+++ scripts/snake.js
@@ -0,0 +1,19 @@
+class snake {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.tailLength = 2;
+        this.tailPos = [
+            [x - 1, y],
+            [x - 2, y]
+        ];
+    }
+
+    draw() {
+
+    }
+
+    move() {
+
+    }
+}
