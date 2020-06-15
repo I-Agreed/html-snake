93466e30eb9fd1c265953326e977a53dcf3fc21f
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2020-06-15 11:09:27 +1000

    finished tail movement code

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,3 +1,7 @@
+const gridSize = 15;
+const snakeColour = "#ff0000"
+const foodColour = "#00ff00"
+var ctx = display.getContext("2d");
 class snake {
     constructor(x, y) {
         this.x = x;
@@ -9,11 +13,29 @@ class snake {
         ];
     }
 
-    draw() {
-
+    draw(food) {
+        var squareSize = display.width / gridSize;
+        var i;
+        var x;
+        var y;
+        ctx.clearRect(0, 0, display.width, display.height);
+        ctx.fillRect(this.x * squareSize, this.y * squareSize, squareSize, squareSize);
+        for (i = 0; i < this.tailPos.length; i++) {
+            x = this.tailPos[i][0];
+            y = this.tailPos[i][1];
+            x = this.tailPos[i][0];
+            ctx.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
+        }
     }
 
-    move() {
+    move(x, y) {
+        this.tailPos.splice(0, 0, [this.x, this.y]);
+        this.x += x;
+        this.y += y;
+        this.tailPos.pop();
 
     }
 }
+
+var player = new snake(5, 5);
+player.draw(1);
